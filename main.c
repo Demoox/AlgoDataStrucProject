bf8be9291e02e04b634ee936f87dcc6c752478c5
Nicola Galluccio <galluccio.nicola@gmail.com>
2021-04-02 18:52:23 +0200

    Add provided code from teacher

--- main.c
+++ main.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include <time.h>
 
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+
+
+long getResolution() {                        // fare attenzione alle divisioni su valori di tipo intero... 
+    struct timespec start, end;
+    clock_gettime(CLOCK_MONOTONIC, &start);
+    do {
+        clock_gettime(CLOCK_MONOTONIC, &end);
+    } while(start.tv_nsec == end.tv_nsec);
+    return (end.tv_nsec - start.tv_nsec);
 }
