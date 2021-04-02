2f999049a81ae7de785245b9bf1b42a2b939877f
Nicola Galluccio <galluccio.nicola@gmail.com>
2021-04-02 18:26:38 +0200

    Revert "Prova 2"
    This reverts commit ced0a6cf105762ef6819e68e61d615986b48db2e.

--- main.c
+++ main.c
@@ -3,8 +3,11 @@
 int main() {
     printf("Hello, World!\n");
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
     printf("Galluccio e' sexy");
 >>>>>>> ced0a6cf105762ef6819e68e61d615986b48db2e
+=======
+>>>>>>> parent of ced0a6c (Prova 2)
     return 0;
 }
