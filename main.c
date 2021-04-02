ff85fcccd223946e74692969de5b8c5640d4c7e7
Simone Gaggio <simone.gaggio@libero.it>
2021-04-02 19:11:18 +0200

    Reformatted code

--- main.c
+++ main.c
@@ -1,18 +1,22 @@
 #include <stdio.h>
 #include <time.h>
 
-int main() {
+long getResolution();
+
+int main()
+{
+    printf("%lu\n", getResolution());
     printf("Hello, World!\n");
     return 0;
 }
 
-
-
-long getResolution() {                        // fare attenzione alle divisioni su valori di tipo intero... 
+long getResolution()
+{
     struct timespec start, end;
     clock_gettime(CLOCK_MONOTONIC, &start);
-    do {
+    do
+    {
         clock_gettime(CLOCK_MONOTONIC, &end);
-    } while(start.tv_nsec == end.tv_nsec);
+    } while (start.tv_nsec == end.tv_nsec);
     return (end.tv_nsec - start.tv_nsec);
 }
