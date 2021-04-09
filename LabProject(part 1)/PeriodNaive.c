a7eb000169aeba29f3e5ae21fd21617a851cb4d8
Nicola Galluccio <galluccio.nicola@gmail.com>
2021-04-09 11:28:45 +0200

    add first concept of periodNaive

--- LabProject(part 1)/PeriodNaive.c
+++ LabProject(part 1)/PeriodNaive.c
@@ -1,8 +1,27 @@
 #include <stdio.h>
+#include <string.h>
+#include <time.h>
 
 int main(){
     
-
-
+    char S[1000];
+    scanf("%999s", S);
+    int p=periodNaive(S);
+    printf("%d", p);
     return 0;
+}
+
+
+int periodNaive(char *S){
+
+    int n = strlen(S);
+    for(int p=1; p<=n;p++){
+        for(int i=1; i<=n; i++){
+            if(S[i] == S[i+p]){
+                return p;
+                } else {
+                    return 0;
+                }
+            }
+        }
 }
