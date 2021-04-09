a069b42710436c26708216f41e350fbde03a5c8d
Simone Gaggio <simone.gaggio@libero.it>
2021-04-09 15:14:44 +0200

    Add function to calculate n

--- LabProject(part 1)/StringCreator.c
+++ LabProject(part 1)/StringCreator.c
@@ -1,9 +1,30 @@
 #include <stdio.h>
 #include <string.h>
 #include <time.h>
+#include <math.h>
+#define A 1000           //lunghezza minima
+#define MAXLENGTH 500000 //lunghezza massima
 
-int main(){
-    
+void firstMethod();
+
+int main()
+{
+
+    double B = exp((log(MAXLENGTH) - log(A)) / 99);
+
+    for (int j = 0; j < 99; j++)
+    {
+
+        double n = A * pow(B, j);
+        //printf("%f\n", n);      test
+
+    }
 
     return 0;
 }
+
+void firstMethod(){
+
+}
+
+//ctrl + shift + i
