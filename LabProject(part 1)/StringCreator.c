7293fe026591a0d666bcb7928e52fedd003666e1
Nicola Galluccio <galluccio.nicola@gmail.com>
2021-04-09 15:52:31 +0200

    add secondMethod for String Gen

--- LabProject(part 1)/StringCreator.c
+++ LabProject(part 1)/StringCreator.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 #include <time.h>
 #include <math.h>
@@ -6,24 +7,51 @@
 #define MAXLENGTH 500000 //lunghezza massima
 
 void firstMethod();
+void secondMethod();
 
 int main()
 {
-
     double B = exp((log(MAXLENGTH) - log(A)) / 99);
+    double n;
 
     for (int j = 0; j < 99; j++)
     {
-
-        double n = A * pow(B, j);
+        n = A * pow(B, j);
         //printf("%f\n", n);      test
-
+        
     }
+    
+    
+    char S[(int) floor(n)];
+    secondMethod((int) floor(n),S);
 
-    return 0;
+    S[(int) floor(n)] = '\0';
+    printf("%s\n\n\n", S);
+
+    
+
+        return 0;
 }
 
 void firstMethod(){
+
+}
+
+
+
+
+void secondMethod(int n, char *S){
+    int q = (rand()%n + 1 );
+
+    for(int i = 0; i<q;i++){
+        S[i] = ((rand()%2) + 'a');
+    }
+
+    for(int i = q; i<n; i++){
+        S[i] = S[(i-1)%q + 1];
+    }
+
+
 
 }
 
