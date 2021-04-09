2613bb0ca4b3fde473b18118b19d36fc68e9205e
Simone Gaggio <simone.gaggio@libero.it>
2021-04-09 16:32:49 +0200

    Update PeriodSmart

--- LabProject(part 1)/PeriodSmart.c
+++ LabProject(part 1)/PeriodSmart.c
@@ -1,18 +1,5 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
-
-int periodSmart(char *S);
-
-int main(){
-
-  char S[1000];
-  scanf("%999s", S);
-  int p=periodSmart(S);
-  printf("%d", p);
-  return 0;
-
-}
 
 int periodSmart(char *S){
    
@@ -21,10 +8,12 @@ int periodSmart(char *S){
    r[0]=0;
    for(int i=0; i<=n-2; i++){ 
       //calcolo r[i+1]
-      int z=r[i];
-      while (z>0 && S[z]!=S[i+1])   //r[i] = lunghezza del primo bordo del primo bordo di S[0..i], S[i] compreso
+      int z=r[i];  //r[i] = lunghezza del primo bordo del primo bordo di S[0..i], S[i] compreso
+      while (z>0 && S[z]!=S[i+1])   
       {
-          z=r[z];   
+          z=r[z-1];   //se il carattere successivo all' ultimo carattere del bordo di lunghezza z non coincide con S[i+1]
+                      //allora all' iterazione successiva considererò come bordo, il primo bordo del bordo di lunghezza z.
+                      //r[z-1] sarebbe il primo bordo del bordo di lunghezza z
       }
       if (S[z]==S[i+1])  //metto r[i] al posto di r[i]+1 in quanto partendo da 0,  la lunghezza del bordo 
                             //sarà gia l'indice del carattere successivo all'ultimo carattere del bordo
@@ -38,3 +27,14 @@ int periodSmart(char *S){
     }
     return n-r[n-1];
 }
+
+
+int main(){
+  char S[1000];
+  scanf("%999s", S);
+  int p=periodSmart(S);
+  printf("%d", p);
+  return 0;
+
+}
+
