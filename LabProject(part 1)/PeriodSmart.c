c91b3a03bfe97e3e6d32df5e21e6757eb332737b
Simone Gaggio <simone.gaggio@libero.it>
2021-04-09 16:36:32 +0200

    Indetation of periodSmart

--- LabProject(part 1)/PeriodSmart.c
+++ LabProject(part 1)/PeriodSmart.c
@@ -1,40 +1,44 @@
 #include <stdio.h>
 #include <string.h>
 
-int periodSmart(char *S){
-   
-   int n=strlen(S);
-   int r[n]; //r[0..n-1]  
-   r[0]=0;
-   for(int i=0; i<=n-2; i++){ 
-      //calcolo r[i+1]
-      int z=r[i];  //r[i] = lunghezza del primo bordo del primo bordo di S[0..i], S[i] compreso
-      while (z>0 && S[z]!=S[i+1])   
-      {
-          z=r[z-1];   //se il carattere successivo all' ultimo carattere del bordo di lunghezza z non coincide con S[i+1]
-                      //allora all' iterazione successiva considererò come bordo, il primo bordo del bordo di lunghezza z.
-                      //r[z-1] sarebbe il primo bordo del bordo di lunghezza z
-      }
-      if (S[z]==S[i+1])  //metto r[i] al posto di r[i]+1 in quanto partendo da 0,  la lunghezza del bordo 
-                            //sarà gia l'indice del carattere successivo all'ultimo carattere del bordo
-                        
-      {
-          r[i+1]=z+1;
-      }else{        
+int periodSmart(char *S);
 
-          r[i+1]=0;
-      }
+int main()
+{
+    char S[1000];
+    scanf("%999s", S);
+    int p = periodSmart(S);
+    printf("%d", p);
+    return 0;
+}
+
+int periodSmart(char *S)
+{
+
+    int n = strlen(S);
+    int r[n]; //r[0..n-1]
+    r[0] = 0;
+    for (int i = 0; i <= n - 2; i++)
+    {
+        //calcolo r[i+1]
+        int z = r[i]; //r[i] = lunghezza del primo bordo del primo bordo di S[0..i], S[i] compreso
+        while (z > 0 && S[z] != S[i + 1])
+        {
+            z = r[z - 1]; //se il carattere successivo all' ultimo carattere del bordo di lunghezza z non coincide con S[i+1]
+                          //allora all' iterazione successiva considererò come bordo, il primo bordo del bordo di lunghezza z.
+                          //r[z-1] sarebbe il primo bordo del bordo di lunghezza z
+        }
+        if (S[z] == S[i + 1]) //metto r[i] al posto di r[i]+1 in quanto partendo da 0,  la lunghezza del bordo
+                              //sarà gia l'indice del carattere successivo all'ultimo carattere del bordo
+
+        {
+            r[i + 1] = z + 1;
+        }
+        else
+        {
+
+            r[i + 1] = 0;
+        }
     }
-    return n-r[n-1];
+    return n - r[n - 1];
 }
-
-
-int main(){
-  char S[1000];
-  scanf("%999s", S);
-  int p=periodSmart(S);
-  printf("%d", p);
-  return 0;
-
-}
-
