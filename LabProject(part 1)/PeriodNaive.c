5565f40686f98f9db4cfb8eb2b16592edd36c51c
LorenzoFurlan <artist.frhl@gmail.com>
2021-04-11 17:09:18 +0200

    add method two

--- LabProject(part 1)/PeriodNaive.c
+++ LabProject(part 1)/PeriodNaive.c
@@ -1,27 +1,30 @@
 #include <stdio.h>
 #include <string.h>
-#include <time.h>
-
-int main(){
-    
-    char S[1000];
-    scanf("%999s", S);
-    int p=periodNaive(S);
-    printf("%d", p);
-    return 0;
-}
 
 
 int periodNaive(char *S){
+     int n= strlen(S);
+     for(int p=1; p<=n; p++){ //forse n-1
+         char *s1=substring(S,0,n-p);
+         char *s2= substring(S,p,n-p);
+         if(strcmp(s1,s2)==0){return p;}
+            
+     }
+     return n;
+     
 
-    int n = strlen(S);
-    for(int p=1; p<=n;p++){
-        for(int i=1; i<=n; i++){
-            if(S[i] == S[i+p]){
-                return p;
-                } else {
-                    return 0;
-                }
-            }
-        }
 }
+
+
+int main(){
+  char S[1000];
+  scanf("%999s", S);
+  int p=periodNaive(S);
+  printf("%d", p);
+  return 0;
+
+}
+
+
+
+
