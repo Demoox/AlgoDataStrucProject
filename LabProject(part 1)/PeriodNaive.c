3ffca78985f2d06a3b24dc038d8e3a751aadcef6
Nicola Galluccio <galluccio.nicola@gmail.com>
2021-04-11 17:24:16 +0200

    fix method1

--- LabProject(part 1)/PeriodNaive.c
+++ LabProject(part 1)/PeriodNaive.c
@@ -3,12 +3,11 @@
 
 int periodNaiveMethod1(char *S)
 {
-    int counter = 0;
     int n = strlen(S);
     for (int p = 0; p < n; p++)
     {
         for (int i = 0; i < n - p; i++)
-            if (S[i] != S[i + p])
+            if (S[i] == S[i + p])
             {
                 return p+i;
             }
