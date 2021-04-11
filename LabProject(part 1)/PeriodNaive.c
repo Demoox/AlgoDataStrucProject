f6e2576be3c59af035d8443389fa8ab08a6c8d94
Nicola Galluccio <galluccio.nicola@gmail.com>
2021-04-11 16:52:16 +0200

    Fix first "for" loop

--- LabProject(part 1)/PeriodNaive.c
+++ LabProject(part 1)/PeriodNaive.c
@@ -18,12 +18,12 @@ int periodNaiveMethod1(char *S)
 {
     int counter = 0;
     int n = strlen(S);
-    for (int p = 0; p <= n; p++)
+    for (int p = 0; p < n; p++)
     {
         for (int i = 0; i < n - p; i++)
             if (S[i] != S[i + p])
             {
-                return p;
+                return p+i;
             }
     }
     
