3b4c624ab13cc9c879ec4f7b9ceb678340721928
Simone Gaggio <simone.gaggio@libero.it>
2021-04-12 11:54:29 +0200

    Tolto dal tempo totale il tempo di creazione della stringa

--- LabProject(part 1)/StringCreator.c
+++ LabProject(part 1)/StringCreator.c
@@ -15,7 +15,7 @@ void thirdMethod();
 long getResolution();
 void plot();
 
-int main()
+/*int mainoriginale()//nel calcolo del tempo viene incluso anche il tempo di creazione della stringa
 {
     double B = exp((log(MAXLENGTH) - log(A)) / 99);
     double n;
@@ -40,8 +40,8 @@ int main()
         {
             S = malloc((int)floor(n) + 1); //TODO: controlla uso memoria
 
-            firstMethod((int)floor(n), S);
-            //secondMethod((int) floor(n),S);
+            //firstMethod((int)floor(n), S);
+            secondMethod((int) floor(n),S);
             //thirdMethod((int) floor(n),S);
 
             //printf("%s\n\n\n", S);    //Stampa stringhe generate
@@ -60,6 +60,64 @@ int main()
 
         //printf("%lu\n", tn);
         y[j]=tn;
+    }
+
+    plot(y);
+
+    return 0;
+}*/
+
+int main()
+{
+    double B = exp((log(MAXLENGTH) - log(A)) / 99);
+    double n;
+
+    char *S = NULL;
+
+    double y[100];
+
+    for (int j = 0; j < 99; j++)
+    {
+        n = A * pow(B, j);
+
+        struct timespec start, end;
+
+        long R = getResolution();
+
+        int k = 0;
+
+        int tempo = 0;
+
+        do
+        {
+            S = malloc((int)floor(n) + 1); //TODO: controlla uso memoria
+
+            firstMethod((int)floor(n), S);
+            //secondMethod((int) floor(n),S);
+            //thirdMethod((int) floor(n),S);
+
+            //printf("%s\n\n\n", S);    //Stampa stringhe generate
+
+            clock_gettime(CLOCK_MONOTONIC, &start);
+
+            periodSmart(S);
+
+            clock_gettime(CLOCK_MONOTONIC, &end);
+
+            free(S);
+
+            k++;
+
+            tempo += end.tv_nsec - start.tv_nsec;
+
+            printf("%d\n", tempo);
+
+        } while (tempo < ((R / Emax) + R));
+
+        long tn = tempo / k; //tempo medio per lunghezza n
+
+        //printf("%lu\n", tn);
+        y[j] = tn;
     }
 
     plot(y);
@@ -131,7 +189,15 @@ void plot(double *y)
 {
 
     char *commandsForGnuplot[] = {"set title \"Metodo 1\"", "set xlabel \"j\"", "set ylabel \"time\"", "set style line 1 \\ linecolor rgb '#0060ad' \\ linetype 1 linewidth 2 \\ pointtype 7 pointsize 1.5", "plot 'data' with linespoints linestyle 1"};
-    
+
+    char *commands[] = {
+        "set title \"Period Smart\"",
+        "set xlabel \"j\"",
+        "set ylabel \"time\"",
+        "set autoscale",
+        /*"set logscale",*/
+        "plot 'data' with lines"};
+
     FILE *temp = fopen("data", "w");
     /*Opens an interface that one can use to send commands as if they were typing into the
      *     gnuplot command line.  "The -persistent" keeps the plot open even after your
@@ -141,14 +207,13 @@ void plot(double *y)
     int i;
     for (i = 0; i < 99; i++)
     {
-        fprintf(temp, "%d %lf \n", i+1, y[i]); //Write the data to a temporary file
+        fprintf(temp, "%d %lf \n", i + 1, y[i]); //Write the data to a temporary file
     }
 
     for (i = 0; i < 5; i++)
     {
-        fprintf(gnuplotPipe, "%s \n", commandsForGnuplot[i]); //Send commands to gnuplot one by one.
+        fprintf(gnuplotPipe, "%s \n", commands[i]); //Send commands to gnuplot one by one.
     }
-
 }
 
 //ctrl + shift + i auto-indent
