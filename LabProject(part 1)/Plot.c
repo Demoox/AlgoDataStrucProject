f7f091e87a69a348110d0340f7ef39beae1af0ca
Simone Gaggio <simone.gaggio@libero.it>
2021-04-19 17:57:10 +0200

    aggiunto plot(x,y)

--- LabProject(part 1)/Plot.c
+++ LabProject(part 1)/Plot.c
@@ -32,3 +32,28 @@ void plot(double *x, double *y, char *title)
         fprintf(gnuplotPipe, "%s \n", commands[i]); //Send commands to gnuplot one by one.
     }
 }
+
+void plot(double *x, double *y)
+{
+
+    char *commands[] = {
+        "set xlabel \"j\"",
+        "set ylabel \"time\"",
+        /*"set autoscale",*/
+        "set logscale",
+        "plot 'data' with lines"
+        /*"plot 'data' with linespoints pointtype 7"*/};
+
+    FILE *temp = fopen("data", "w");
+    FILE *gnuplotPipe = popen("gnuplot -persistent", "w");
+    int i;
+    for (i = 0; i < 99; i++)
+    {
+        fprintf(temp, "%lf %lf \n", x[i], y[i]); //Write the data to a temporary file
+    }
+
+    for (i = 0; i < 4; i++)
+    {
+        fprintf(gnuplotPipe, "%s \n", commands[i]); //Send commands to gnuplot one by one.
+    }
+}
