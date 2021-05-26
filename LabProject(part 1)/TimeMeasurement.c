c0e9e49f4a378251ff9d13d256418476dfc73068
Simone Gaggio <simone.gaggio@libero.it>
2021-05-26 11:15:45 +0200

    timeMeasurement commentato

--- LabProject(part 1)/TimeMeasurement.c
+++ LabProject(part 1)/TimeMeasurement.c
@@ -17,110 +17,109 @@
 
 int main()
 {
-    int firstChoice, secondChoice;
+    int methodChoice, algorithmChoice;
     printf("Scegli un metodo per creare la stringa:\n");
     printf("1- Generazione pseudo-casuale\n");
     printf("2- s(i) = s((i-1) mod q+1)\n");
     printf("3- Variante del metodo 3 con aggiunta di lettera c\n");
     printf("Metodo da usare: ");
-    scanf("%d", &firstChoice);
+    scanf("%d", &methodChoice);
+
     printf("\nScegi quale funzione usare:\n");
     printf("1- PeriodNaive\n");
     printf("2- PeriodSmart\n");
     printf("Funzione da usare: ");
-    scanf("%d", &secondChoice);
-
-    //-----------------------------------------------
+    scanf("%d", &algorithmChoice);
 
     double B = exp((log(MAXLENGTH) - log(A)) / 99);
-    double n;
-    int measurePrec = 50;
+    double n;            //lunghezza stringa
+    int measurePrec = 50; //indice di precisione della misura
 
-    char *S = NULL;
+    char *S = NULL; //stringa generata
 
-    double x[100];
-    double y[100];
+    double x[100]; //array contenente le lunghezza della stringa alla j-esima iterazione
+    double y[100]; //array contenente il tempo impiegato dall'algoritmo alla j-esima iterazione
+    double tn = 0; //tempo medio misurato
 
-    double tn = 0;
-
-    double *strGenTime;
-    strGenTime = StringsGenerationTime(firstChoice);
+    double *strGenTime;                               //array contenente i tempi medi impiegati per generare le strighe
+    strGenTime = StringsGenerationTime(methodChoice); //strGenTime[j] = tempo medio misurato per generare la stringa di lunghezza n=floor(A*B^j)
 
     for (int j = 0; j <= 99; j++)
     {
         n = A * pow(B, j);
-        double time_sum=0;
-        for(int w=0; w<measurePrec; w++){
-         struct timespec start, end;
+        double time_sum = 0; //somma dei tempi medi misurati
 
-         long R = getResolution() / (double)BILLION;
-
-         int k = 0;
-
-         double tempo = 0;
-
-         //if (tn < (double)2)
-         //{
-            clock_gettime(CLOCK_MONOTONIC, &start);
-            do
+        if (tn < (double)2) //cap di tempo massimo di 2 secondi impostato per period naive
+        {
+            for (int w = 0; w < measurePrec; w++) //ciclo per fare la media dei tempi medi misurati
             {
-                S = malloc((int)floor(n) + 1); //TODO: controlla uso memoria
+                struct timespec start, end;
 
-                switch (firstChoice)
+                long R = getResolution() / (double)BILLION;
+
+                int k = 0;
+
+                double tempo = 0;
+
+                clock_gettime(CLOCK_MONOTONIC, &start);
+
+                do
                 {
-                case 1:
-                    firstMethod((int)floor(n), S);
-                    break;
-                case 2:
-                    secondMethod((int)floor(n), S);
-                    break;
-                case 3:
-                    thirdMethod((int)floor(n), S);
-                    break;
-                default:
-                    printf("Valore non valido.\n");
-                    break;
-                }
+                    S = malloc((int)floor(n) + 1); //allocamento dinamico di memoria per risparmiare risorse
 
-                switch (secondChoice)
-                {
-                case 1:
-                    //periodNaiveMethod1(S);
-                    periodNaiveMethod2(S);
-                    clock_gettime(CLOCK_MONOTONIC, &end);
-                    break;
-                case 2:
-                    periodSmart(S);
-                    clock_gettime(CLOCK_MONOTONIC, &end);
-                    break;
-                default:
-                    printf("Valore non valido.\n");
-                    break;
-                }
+                    switch (methodChoice)
+                    {
+                    case 1:
+                        firstMethod((int)floor(n), S);
+                        break;
+                    case 2:
+                        secondMethod((int)floor(n), S);
+                        break;
+                    case 3:
+                        thirdMethod((int)floor(n), S);
+                        break;
+                    default:
+                        printf("Valore non valido.\n");
+                        break;
+                    }
 
-                free(S);
+                    switch (algorithmChoice)
+                    {
+                    case 1:
+                        //periodNaiveMethod1(S);
+                        periodNaiveMethod2(S);
+                        clock_gettime(CLOCK_MONOTONIC, &end);
+                        break;
+                    case 2:
+                        periodSmart(S);
+                        clock_gettime(CLOCK_MONOTONIC, &end);
+                        break;
+                    default:
+                        printf("Valore non valido.\n");
+                        break;
+                    }
 
-                k++;
-                
-                tempo = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / (double)BILLION;
-            } while (tempo < ((R / Emax) + R));
-            time_sum += tempo / k;
-            //tn = (tempo / k) - strGenTime[j];
-            
-          //}
+                    free(S);
+
+                    k++;
+
+                    tempo = (end.tv_sec - start.tv_sec) + (end.tv_nsec - start.tv_nsec) / (double)BILLION; //tempo in secondi
+
+                } while (tempo < ((R / Emax) + R));
+
+                time_sum += tempo / k; //calcolo del tempo medio per generare la stringa di lunghezza n e di esecuzione dell'algoritmo
+            }
+
+            tn = (time_sum / measurePrec) - strGenTime[j]; //tempo medio di esecuzione dell'algoritmo per una stringa du lunghezza n
         }
-        tn=(time_sum/measurePrec) - strGenTime[j];
-        printf("%i   %lf\n", (int)floor(n), tn);
-        
+
+        printf("%i   %lf\n", (int)floor(n), tn); //stampa a video la combinata della lunghezza della stringa e del tempo impiegato a processarla
+
         x[j] = n;
         y[j] = tn;
-    
-    
-    
     }
-    //-----------------------------------------------
 
-    plot(x, y, secondChoice, firstChoice);
+    plot(x, y, algorithmChoice, methodChoice); //funzione che stampa il grafico a fine esecuzione
 
     return 0;
 }
